ad9461c9d992520301adc8ecc489a8b783f506da
Michael Sullivan <sullivanmichael344@gmail.com>
2023-12-27 22:29:12 -0500

    Add event listener to buttons

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const playerScoreDisplay = document.querySelector(".playerscore")
 const computerScoreDisplay = document.querySelector(".computerscore")
 
 rock.addEventListener("click", () =>{
+    
     let computerChoice = getComputerChoice();
     if (computerChoice == "Rock"){
         resultsRound.textContent = "Computer chose Rock, it's a tie!"
@@ -38,6 +39,42 @@ rock.addEventListener("click", () =>{
 
     } else if(computerChoice == "Scissors"){
         resultsRound.textContent = "Computer chose Scissors, you win!"
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore
+    }
+})
+
+paper.addEventListener("click", () =>{
+    let computerChoice = getComputerChoice();
+    if (computerChoice == "Paper"){
+        resultsRound.textContent = "Computer chose Paper, it's a tie!"
+
+    } else if(computerChoice == "Scissors"){
+        resultsRound.textContent = "Computer chose Scissors, you lose!"
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore
+
+
+    } else if(computerChoice == "Rock"){
+        resultsRound.textContent = "Computer chose Rock, you win!"
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore
+    }
+})
+
+scissors.addEventListener("click", () =>{
+    let computerChoice = getComputerChoice();
+    if (computerChoice == "Scissors"){
+        resultsRound.textContent = "Computer chose Scissors, it's a tie!"
+
+    } else if(computerChoice == "Rock"){
+        resultsRound.textContent = "Computer chose Rock, you lose!"
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore
+
+
+    } else if(computerChoice == "Paper"){
+        resultsRound.textContent = "Computer chose Paper, you win!"
         playerScore++;
         playerScoreDisplay.textContent = playerScore
     }
